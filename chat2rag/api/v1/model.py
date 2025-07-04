7a6afac022b054375e4f30a3e36684e94a394b5c
Jacob Zheng <1453366421@qq.com>
2025-07-04 17:29:57 +0800

    feat: 简易添加 mode l映射

--- chat2rag/api/v1/model.py
+++ chat2rag/api/v1/model.py
@@ -12,12 +12,6 @@ def get_model_list():
     """
     获取模型列表
     """
-    response = requests.get(
-        f"{CONFIG.OPENAI_BASE_URL}/models",
-        headers={"Authorization": "Bearer " + CONFIG.OPENAI_API_KEY},
-    )
-    print({"Authorization": "Bearer " + CONFIG.OPENAI_API_KEY})
-    if response.status_code == 200:
-        return Success(data=response.json()["data"])
-    else:
-        return Error(msg="获取模型列表失败", data=response.json())
+    return Success(data=CONFIG.MODEL_LIST)
+    # else:
+    #     return Error(msg="获取模型列表失败", data=response.json())
